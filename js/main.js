88b6b7886a791766bf7e9de776db65b4866e23b2
milly-y <milenna.milly.nunes2716@gmail.com>
2018-11-20 11:54:01 -0300

    Comecei a página de login

--- js/main.js
+++ js/main.js
@@ -32,7 +32,16 @@ $(document).ready(function(){
 
   $('.nav-item a:first').tab('show');
 
+  console.log($("input[defaultPlaceholder]").attr('.defaultPlaceholder');
+  $("input[defaultPlaceholder]").on({
+    click: function(){
+      this.placeholder = '';
+    },
+    blur: function(){
 
+      this.placeholder = this.defaultPlaceholder;
+    }
+  });
 
   // Select all links with hashes
   $('.navbar a[href*="#"]')
